569cc746272636c2cd76508d138e6eeb3604d737
onurgul46 <34624508+onurgul46@users.noreply.github.com>
2017-12-17 16:53:05 +0300

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string ad; int u, t;
+            Console.WriteLine("İsminizi girin:"); ad = Console.ReadLine();
+            u = ad.Length;
+            for (int i = 0; i < u; i++)
+            {
+                t = (int)ad[i] + 10;
+                Console.Write((char)t);
+            }
+            Console.ReadLine();
+           
+          
+               
+        }
+    }
+}
